4f248ce1c839a9eeb8c334b62c80e005fc9cac49
gbacoding3060 <gbacoding@gmail.com>
2023-08-09 13:56:17 +0200

    Classes and subclasses

---
+++ Tic-tac-toe Game/Players/Bot.java
@@ -0,0 +1,8 @@
+package Players;
+
+public class Bot extends Players {
+
+    String symbol = "o";
+    String name = "Bot";
+    
+}
---
+++ Tic-tac-toe Game/Players/User.java
@@ -0,0 +1,20 @@
+package Players;
+
+import java.util.Scanner;
+
+public class User extends Players {
+
+
+
+    // Constructor Scanner Class
+    Scanner src = new Scanner(System.in);
+    System.out.println("Introduce your name.");
+    symbol = src.nextLine();
+    Players.setName(symbol);
+
+
+    // DATA INPUT
+
+
+
+}
