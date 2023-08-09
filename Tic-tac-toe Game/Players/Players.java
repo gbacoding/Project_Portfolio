2af451c45c34523a5d27d5ed4ae8b94e1e3c2dcb
gbacoding3060 <gbacoding@gmail.com>
2023-08-09 13:55:07 +0200

    Class Players & Subclasses User and Bot

---
+++ Tic-tac-toe Game/Players/Players.java
@@ -0,0 +1,23 @@
+package Players;
+
+public class Players {
+
+    private String symbol;
+    private String name;
+    
+
+    Players() {
+        
+    }
+
+    //Setter method to set name
+    void setName(String name){
+        this.name = name;
+    }
+
+    //Getter method to set name
+
+    String getName(){
+        return name;
+    }
+}
