6fbf36c7fc1203f5441acd5d0fdb06c02cb7a644
gbacoding3060 <gbacoding@gmail.com>
2023-08-23 13:18:05 +0200

    Development App Class

--- Tic_Tac_Toe/src/App.java
+++ Tic_Tac_Toe/src/App.java
@@ -1,8 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
         
+        // Declare matrix
+        String [][] gameMatrix = new String[3][3];
+        
         // Formacion de la matriz del tablero
-        String [][] gameMatrix = { {"0","0","X"}, {"X","0","X"}, {"0","X","X"} };
 
         // Comprobar si hay una combinación ganadora (a partir del 5º turno (4- 0,1,2,3,4))
 
--- Tic_Tac_Toe/src/Board.java
+++ Tic_Tac_Toe/src/Board.java
@@ -7,13 +7,17 @@ public class Board {
     /** 
      * Displays the current state of the game board on the console 
      */
-    static void displayBoard(String [][] matrix){    
+    static void displayBoard(String [][] matrix){ 
+        System.out.println("BOARD");  
+        System.out.println("-----"); 
         for(String [] row: matrix){
             for (String element: row){
                 System.out.print(element+ " ");
+                
             }
             System.out.println();
+       
         }
     }
-    
+
 }
